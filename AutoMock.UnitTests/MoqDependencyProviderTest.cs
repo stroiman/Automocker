1f49b4899dede0277ee7bea69f9f52e4f08d9904
unknown <peter@.(none)>
2011-03-30 11:38:07 +0200

    Created MoqInstanceProvider

---
+++ AutoMock.UnitTests/MoqDependencyProviderTest.cs
@@ -0,0 +1,24 @@
+﻿using AutoMock.UnitTests.TestClasses;
+using Moq;
+using NUnit.Framework;
+
+namespace AutoMock.UnitTests
+{
+    [TestFixture]
+    public class MoqDependencyProviderTest
+    {
+        [Test]
+        public void ResolvingAnInterfaceShouldReturnAMoq()
+        {
+            // Setup
+            var repository = new MoqDependencyProvider();
+
+            // Exercise
+            var mockedInstance = repository.GetInstance<ISimpleDependency>();
+
+            // Verify
+            var mock = Mock.Get(mockedInstance);
+            Assert.That(mock, Is.Not.Null);
+        }
+    }
+}
---
+++ AutoMock/MoqDependencyProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+
+namespace AutoMock
+{
+    /// <summary>
+	/// A specialized <see cref="IDependencyProvider"/> that works by returning
+    /// <see cref="Mock"/> objects.
+    /// </summary>
+    public class MoqDependencyProvider : IDependencyProvider
+    {
+        /// <summary>
+        /// Resolves an instance of <typeparamref name="T"/>
+        /// </summary>
+        public T GetInstance<T>() where T : class
+        {
+            return new Mock<T>().Object;
+        }
+    }
+}
