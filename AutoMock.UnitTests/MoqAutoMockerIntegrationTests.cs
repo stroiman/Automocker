78132590c735241f250580290460e3d4cc9f4d05
unknown <peter@.(none)>
2011-03-30 11:38:21 +0200

    Created MoqAutoMocker for automocking with Moq

---
+++ AutoMock.UnitTests/MoqAutoMockerIntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMock.UnitTests.TestClasses;
+using Moq;
+using NUnit.Framework;
+
+namespace AutoMock.UnitTests
+{
+	/// <summary>
+	/// The MoqAutoMocker class integrates all the components for creating a auto mocker
+	/// using Moq as the mocking tool. This test class tests that the integration works
+	/// correctly.
+	/// </summary>
+	[TestFixture]
+	public class MoqAutoMockerIntegrationTests
+	{
+		[Test]
+		public void ClassWithDependencyShouldBeConstructedWithMoq()
+		{
+			// Setup
+			var automocker = new MoqAutoMocker();
+
+			// Exercise
+			var instance = automocker.GetInstance<ClassWithSimpleDependency>();
+
+			// Verify
+			var dependencyMock = Mock.Get(instance.Dependency);
+			Assert.That(dependencyMock, Is.Not.Null);
+		}
+	}
+}
---
+++ AutoMock/MoqAutoMocker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoMock
+{
+	/// <summary>
+	/// A specialized <see cref="AutoMocker"/> that injects <see cref="Moq"/> objects
+	/// into the instance to create.
+	/// </summary>
+	public class MoqAutoMocker : AutoMocker
+	{
+		public MoqAutoMocker() : base(new MoqDependencyProvider())
+		{
+		}
+	}
+}
