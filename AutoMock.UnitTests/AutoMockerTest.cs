df5797e9d994196911a5f65cc0543298c5cc8385
unknown <peter@.(none)>
2011-03-30 11:01:12 +0200

    Refactor AutoMockertest to reduce duplication for new tests

--- AutoMock.UnitTests/AutoMockerTest.cs
+++ AutoMock.UnitTests/AutoMockerTest.cs
@@ -6,11 +6,18 @@ namespace AutoMock.UnitTests
     [TestFixture]
     public class AutoMockerTest
     {
+        private AutoMocker _automocker;
+
+        [SetUp]
+        public void Setup()
+        {
+            _automocker = new AutoMocker();
+        }
+
         [Test]
         public void CreateInstanceWithoutDependencies()
         {
-            var automocker = new AutoMocker();
-            var instance = automocker.GetInstance<ClassWithoutDependencies>();
+            var instance = _automocker.GetInstance<ClassWithoutDependencies>();
             Assert.That(instance, Is.InstanceOf<ClassWithoutDependencies>());
         }
     }
