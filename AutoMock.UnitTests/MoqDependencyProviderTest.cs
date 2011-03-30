b3ec8389975270e9847a9af1c1d0f35cda633159
unknown <peter@.(none)>
2011-03-30 12:05:28 +0200

    Implemented MoqDependencyProvider.GetMock<T>

--- AutoMock.UnitTests/MoqDependencyProviderTest.cs
+++ AutoMock.UnitTests/MoqDependencyProviderTest.cs
@@ -25,5 +25,31 @@ namespace AutoMock.UnitTests
             var mock = Mock.Get(mockedInstance);
             Assert.That(mock, Is.Not.Null);
         }
+
+        [Test]
+        public void ResolvingAfterGetMockShouldReturnSameInstance()
+        {
+            // Setup
+            var mock = _repository.GetMock<ISimpleDependency>();
+
+            // Exercise
+            var mockedInstance = _repository.GetInstance<ISimpleDependency>();
+
+            // Verify
+            Assert.That(mock.Object, Is.SameAs(mockedInstance));
+        }
+
+        [Test]
+        public void GetMockAfterResolvingShouldReturnSameInstance()
+        {
+            // Setup
+            var mockedInstance = _repository.GetInstance<ISimpleDependency>();
+
+            // Exercise
+            var mock = _repository.GetMock<ISimpleDependency>();
+
+            // Verify
+            Assert.That(mockedInstance, Is.SameAs(mock.Object));
+        }
     }
 }
--- AutoMock/MoqDependencyProvider.cs
+++ AutoMock/MoqDependencyProvider.cs
@@ -12,12 +12,28 @@ namespace AutoMock
     /// </summary>
     public class MoqDependencyProvider : IDependencyProvider
     {
+        private readonly Dictionary<Type, Mock> _cache = new Dictionary<Type,Mock>();
+
         /// <summary>
         /// Resolves an instance of <typeparamref name="T"/>
         /// </summary>
         public T GetInstance<T>() where T : class
         {
-            return new Mock<T>().Object;
+            return GetMock<T>().Object;
+        }
+
+        /// <summary>
+        /// Finds a previously created instance; or creates a new instance of
+        /// <see cref="Mock{T}"/> with the specified type of <typeparamref name="T"/>
+        /// </summary>
+        public Mock<T> GetMock<T>() where T : class
+        {
+            Mock cachedMock;
+            if (_cache.TryGetValue(typeof(T), out cachedMock))
+                return (Mock<T>)cachedMock;
+            var mock = new Mock<T>();
+            _cache.Add(typeof(T), mock);
+            return mock;
         }
     }
 }
